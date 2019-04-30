7608a360be42768340816fd0cf774291272bb933
Levi Paulk <levipaulk@gmail.com>
2019-04-30 16:17:30 -0400

    all dependencies install

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const movies = require('./movies.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
