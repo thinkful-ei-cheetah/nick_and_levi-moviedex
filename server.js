1a56898991776a9b1be257007cb9925b23778378
Nick Lee <leexnick@gmail.com>
2019-05-06 19:51:31 -0700

    fix for heroku deploy binding to localhost

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
